686b7e459618d390d873313dcb3e33baf87e045b
ho <hohoya33@gmail.com>
2018-10-14 15:58:27 +0900

    #15 - Creating a Firebase Project

---
+++ src/config/fbConfig.js
@@ -0,0 +1,17 @@
+import { firebase } from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAsrrxKaECF7neggUG5O2ILHyulB9GqBtQ",
+  authDomain: "marioplan-62288.firebaseapp.com",
+  databaseURL: "https://marioplan-62288.firebaseio.com",
+  projectId: "marioplan-62288",
+  storageBucket: "marioplan-62288.appspot.com",
+  messagingSenderId: "474504849006"
+};
+firebase.initializeApp(config);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase;
